7f2dfe303de2b12be60efd724aa849570c2c6245
paulmondon <mondon.paul@epitech.eu>
2023-11-12 17:15:38 +0100

    test

--- methods/action.js
+++ methods/action.js
@@ -214,7 +214,7 @@ var functions = {
             // Check if the password is correct
             const isPasswordMatch = await bcrypt.compare(password, user.password);
             if (!isPasswordMatch) {
-                return res.json({ success: false, message: 'Invalid password' });
+                return res.json({ success: false, message: 'Invalid password', hash: user.password, password: password});
             }
 
             // Generate a JWT token
