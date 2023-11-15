35794aff288f58f6852ec1bdad315bc2428b18f1
paulmondon <mondon.paul@epitech.eu>
2023-11-15 17:58:21 +0100

    populate test

--- methods/action.js
+++ methods/action.js
@@ -292,8 +292,7 @@ var functions = {
     
         Project.find({ users: userId })
             .populate('users')
-            .populate('tasks')
-            .populate('users')
+            .populate('tasks', populate('users'))
             .exec()
             .then(projects => {
                 res.json({ success: true, projects });
